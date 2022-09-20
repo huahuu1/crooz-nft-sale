ffa8f3b09127d3ebb4108dc4db71bad907ac7ea1
Phuochoit <phuocck8@gmail.com>
2022-09-20 08:55:45 +0700

    add test guzzle Http get data

--- app/Console/Commands/CheckStatusTokenSaleCommand.php
+++ app/Console/Commands/CheckStatusTokenSaleCommand.php
@@ -5,6 +5,10 @@ namespace App\Console\Commands;
 use App\Jobs\UpdateStatusTokenSaleJob;
 use App\Models\TokenSaleHistory;
 use Illuminate\Console\Command;
+use GuzzleHttp\Client as GuzzleClient;
+use Etherscan\APIConf;
+use Etherscan\Client;
+use Illuminate\Support\Facades\Log;
 
 class CheckStatusTokenSaleCommand extends Command
 {
@@ -52,14 +56,46 @@ class CheckStatusTokenSaleCommand extends Command
      */
     public function validateTransactions()
     {
-        $company_wallet = env('COMPANY_WALLET');
-        $contract_wallet = env('CONTRACT_WALLET_USDT');
+        $company_wallet = config('defines.wallet.company_token_sale');
+        $contract_wallet = config('defines.wallet.usdt');
+        $this->checkWithApiScan();
         // run 15 row in 1 min
         $pendingTransactions = $this->transactions->pendingTokenSaleTransactions()->limit(15)->get();
-        if (! empty($pendingTransactions)) {
+        if (!empty($pendingTransactions)) {
             foreach ($pendingTransactions as $key => $transaction) {
                 UpdateStatusTokenSaleJob::dispatch($transaction, $company_wallet, $contract_wallet, $key)->delay(now()->addSeconds(($key + 1) * 3));
             }
         }
+    }
+
+    public function checkWithApiScan()
+    {
+
+        $api_key = config('defines.api.bsc.api_key');
+        $apiConfBsc = APIConf::TESTNET_BSC;
+        $baseUri = config('defines.api.bsc.url');
+
+        $client = new GuzzleClient(
+            [
+                'base_uri' => $baseUri,
+                'headers' => [],
+            ]
+        );
+        $params = [
+            'query' => [
+                'module' => 'proxy',
+                'action' => 'eth_getTransactionByHash',
+                'txhash' => '0xde2ed71997dd8cd7fedf4b4285906b34578b5c62332ae38fd540e5b34043ab23',
+                'apikey' => $api_key,
+            ],
+        ];
+        $uri = '?';
+        $response = $client->request(
+            'GET',
+            $uri,
+            $params
+        );
+        $responseData = json_decode($response->getBody()->getContents(), true);
+        Log::info("check:token-sale responseData::". $response->getBody());
     }
 }
