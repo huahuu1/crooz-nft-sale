46fbd6ac2b11f15ac42cca045f3cf483e66535cd
HuuHT <huuth@kozo-japan.com>
2022-12-16 08:40:16 +0700

    Update code

--- app/Http/Requests/PaymentCouponRequest.php
+++ app/Http/Requests/PaymentCouponRequest.php
@@ -9,16 +9,6 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 class PaymentCouponRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, mixed>
@@ -67,17 +57,5 @@ class PaymentCouponRequest extends FormRequest
             ],
 
         ];
-    }
-
-    /**
-     * Get the error messages for the defined validation rules.*
-     * @return array
-     */
-    protected function failedValidation(Validator $validator)
-    {
-        throw new HttpResponseException(response()->json([
-            'errors' => $validator->errors()->first(),
-            'status' => true
-        ], 422));
     }
 }
