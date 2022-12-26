d484ef49ee5b7f055b14f4f20c3f50207c0cb2e6
HuuHT <huuth@kozo-japan.com>
2022-12-26 15:19:55 +0700

    Update delivery source when used free ticket

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -100,7 +100,7 @@ class TicketController extends Controller
                     $this->auctionNftService->createNftAuction(
                         $request->wallet_address,
                         $nftId,
-                        7,
+                        $request->delivery_source ?? 7,
                         1
                     );
                 }
