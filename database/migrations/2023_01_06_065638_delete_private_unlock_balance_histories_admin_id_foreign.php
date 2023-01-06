d36f85de662614cda563ef6a98e555e22c2c6972
HuuHT <huuth@kozo-japan.com>
2023-01-06 14:21:39 +0700

    [CROOZ_D-166] [backend] delete admin_id forein key of private_unlock_balance_histories table

---
+++ database/migrations/2023_01_06_065638_delete_private_unlock_balance_histories_admin_id_foreign.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('private_unlock_balance_histories', function (Blueprint $table) {
+            $table->dropForeign(['admin_id']);
+            $table->dropColumn('admin_id');
+        });
+        Schema::table('private_unlock_balance_histories', function (Blueprint $table) {
+            $table->unsignedBigInteger('admin_id')->nullable()->after('unlock_token_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('private_unlock_balance_histories', function (Blueprint $table) {
+            $table->foreignId('admin_id')->after('unlock_token_date')->constrained('admins');
+        });
+    }
+};
